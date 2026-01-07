d01dd99f9dc20a577eff72d394ac1cf5a6fc000e
Devansh Dayama <143751642+Devansh23dayama@users.noreply.github.com>
2026-01-07 12:11:01 +0530

    Add files via upload

--- sqlclasses.sql
+++ sqlclasses.sql
@@ -603,3 +603,57 @@ select * from payment where amount >all(select amount from payment where payment
 -- learning assignment 
 -- what are corelated subQuery and how they are executed 
 -- what are constraints in sql 
+
+
+-- for practice not related to table 
+select emp.enmae from employee as emp join department as dept where emp.dept=dept.dept and emp.salary >dept.salary; 
+
+
+
+
+-- Correlated SubQuery :- iiner query takes value from outer query to change result for everytime.alter
+
+create database regexcorr;
+use regexcorr;
+
+CREATE TABLE employee (
+    eid INT PRIMARY KEY,
+    ename VARCHAR(50),
+    salary INT,
+    department_name VARCHAR(50)
+);
+
+INSERT INTO employee (eid, ename, salary, department_name) VALUES
+(1,  'Emp1',  30000, 'HR'),
+(2,  'Emp2',  32000, 'HR'),
+(3,  'Emp3',  34000, 'HR'),
+(4,  'Emp4',  36000, 'HR'),
+(5,  'Emp5',  38000, 'HR'),
+
+(6,  'Emp6',  50000, 'IT'),
+(7,  'Emp7',  52000, 'IT'),
+(8,  'Emp8',  54000, 'IT'),
+(9,  'Emp9',  56000, 'IT'),
+(10, 'Emp10', 58000, 'IT'),
+
+(11, 'Emp11', 60000, 'Finance'),
+(12, 'Emp12', 62000, 'Finance'),
+(13, 'Emp13', 64000, 'Finance'),
+(14, 'Emp14', 66000, 'Finance'),
+(15, 'Emp15', 68000, 'Finance'),
+
+(16, 'Emp16', 40000, 'Sales'),
+(17, 'Emp17', 42000, 'Sales'),
+(18, 'Emp18', 44000, 'Sales'),
+(19, 'Emp19', 46000, 'Sales'),
+(20, 'Emp20', 48000, 'Sales');
+
+select * from employee;
+select avg(salary),department_name from employee group by department_name;
+select ename,salary ,department_name from employee where salary > (select avg(salary) from employee as e 
+where e.department_name=employee.department_name);
+
+-- get emp name , salary and dep name ,jiska khudka salary uske department ki minimum salary ke equal ho ;
+
+select ename,salary,department_name from employee where salary = (select min(salary) from employee as e 
+where e.department_name=employee.department_name);
